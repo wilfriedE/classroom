aa7594ba98d24621b6448512c4bd2e42d6a648ee
Cheng-Yu Hsu <m@cyhsu.me>
2016-12-05 15:36:25 +0800

    Remove uniqueness constraint on commit sha

--- app/models/assignment_submission.rb
+++ app/models/assignment_submission.rb
@@ -3,7 +3,6 @@ class AssignmentSubmission < ApplicationRecord
   belongs_to :submittable, polymorphic: true
 
   validates :sha, presence: true
-  validates :sha, uniqueness: { scope: :submittable_id }
 
   validates :github_release_id, presence: true
   validates :github_release_id, uniqueness: { scope: :submittable_id }
