fda6460fbada6f6089560e5db4245fa2e53b8e3a
Mark Tareshawty <tarebyte@github.com>
2017-02-26 22:53:17 -0500

    Remove spec folder

--- spec/models/student_identifier_type_spec.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-require 'rails_helper'
-
-RSpec.describe StudentIdentifierType, type: :model do
-  it_behaves_like 'a default scope where deleted_at is not present'
-end
