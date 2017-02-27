c1c1682b3d78ba4c3cd6fd9988067eec6b6387cc
Mark Tareshawty <tarebyte@github.com>
2017-02-27 09:59:10 -0500

    Use the correct column

--- test/support/omniauth.rb
+++ test/support/omniauth.rb
@@ -13,7 +13,7 @@ module SignInHelper
   def sign_in_as(user)
     OmniAuth.config.mock_auth[:github].uid = user.uid
     OmniAuth.config.mock_auth[:github].extra.raw_info.site_admin = user.site_admin
-    OmniAuth.config.mock_auth[:github].credentials.token = user.access_token
+    OmniAuth.config.mock_auth[:github].credentials.token = user.token
 
     post '/auth/github/callback'
   end
